17d8020f98a58e063f0ac05be8c55cc50c827a8d
RitaliJ <ritaliaarav@gmail.com>
2022-02-06 12:38:10 -0800

    fixed table styling

--- FoodBank/food.js
+++ FoodBank/food.js
@@ -1,6 +1,6 @@
 
 const zipCodeInput = document.querySelector('#zipCodeInput');
-const foodBanksList = document.querySelector('#foodBanksList');
+const foodBanksTable = document.querySelector('#foodBanksTable');
 const button = document.querySelector('#zipCodeButton');
 const output = document.querySelector('#output');
 
@@ -29,67 +29,88 @@ const fetchFoodBanks = async(lat, long) => {
 
 	}catch(e){
 		console.log("Unable to process the request at this moment", e);
-		clearFoodBanks("Unable to process the request at this moment");
+		clearFoodBanks("Unable to process the request at this moment. Please make sure you have entered a California zipcode.");
 	}
 }
 
-const getFoodBanks = (foodBanksData) => {
-	//rename variables
-	const newTH = document.createElement('TR');
-	const newTDName = document.createElement('TH');
-	newTDName.append("Name");
-	newTH.append(newTDName);
-	const newTDDistance = document.createElement('TH');
-	newTDDistance.append("Distance (miles)");
-	newTH.append(newTDDistance);
-	foodBanksList.append(newTH);
+//function to handle escape characters returned from api
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
 
+const getFoodBanks = (foodBanksData) => {
+	//create table headers
+	const newTR = document.createElement('TR');
+	const newTHName = document.createElement('TH');
+	const newTHDistance = document.createElement('TH');
+	const newTHAddress = document.createElement('TH');
+	const newTHWebsite = document.createElement('TH');
+	newTHName.append("Name");
+	newTHDistance.append("Distance (miles)");
+	newTHAddress.append("Address");
+	newTR.append(newTHName);
+	newTR.append(newTHDistance);
+	newTR.append(newTHAddress);
+	foodBanksTable.append(newTR);
+
+	//add each food bank to the table
 	for(let foodBank of foodBanksData){
-		//escape HTML charcters
-		const name = foodBank.properties.title;
-		//round it to 2 decimal places and append unit
-		const distance = foodBank.properties.distance;
-		// make it a link that is clicable in another tab
+		//manage escape HTML characters using htmlDecode function
+		const name = htmlDecode(foodBank.properties.title);
+		//round distance to 2 decimal places
+		const distance = Math.round(foodBank.properties.distance * 100)/100;
 		const website = foodBank.properties.website;
 		// can we open google map??? embedded and inline??
 		const address = foodBank.properties.address;
 		const city = foodBank.properties.city;
-		const info = name + "\n" + distance + "\n" + website + "\n" + address + ", " + city;
-		console.log(info);
-		addFoodBanks(name, distance);
+		const addressInfo = address + ", " + city;
+		addFoodBanks(name, distance, addressInfo, website);
 	}
+
+	foodBanksTable.style.backgroundColor = "#F5FAFA";
 }
 
-const addFoodBanks = (name, distance) => {
-	//rename variables
-	//apply CSS styling 
-	const newLI = document.createElement('TR');
+const addFoodBanks = (name, distance, address, website) => {
+	const newRow = document.createElement('TR');
 	const newNameTD = document.createElement('TD');
-	newNameTD.append(name);
 	const newDistanceTD = document.createElement('TD');
+	const newAddressTD = document.createElement('TD');
+	// make it a link that is clickable in another tab
+	const a = document.createElement('A');
+	a.append(name)
+	a.href = website;
+	a.target = "_blank";
+	newNameTD.append(a);
 	newDistanceTD.append(distance);
-	newLI.append(newNameTD);
-	newLI.append(newDistanceTD);
-	// newLI.append(info);
-	foodBanksList.append(newLI);
+	newAddressTD.append(address);
+
+	newRow.append(newNameTD);
+	newRow.append(newDistanceTD);
+	newRow.append(newAddressTD);
+	foodBanksTable.append(newRow);
 }
 
 const clearFoodBanks = (message) => {
-	// As long as <ul> has a child node, remove it
-	while (foodBanksList.hasChildNodes()) {  
- 		foodBanksList.removeChild(foodBanksList.firstChild);
+	// As long as the table has a child node, remove it
+	while (foodBanksTable.hasChildNodes()) {  
+ 		foodBanksTable.removeChild(foodBanksTable.firstChild);
 	}
+	foodBanksTable.style.backgroundColor = "white";
 	if(message != ""){
 		const errorMessage = document.createElement('p');
 		errorMessage.append(message);
 		output.append(errorMessage);
-
 	}
 }
 
 button.addEventListener('click', function(e){
 	e.preventDefault();
-	// clear foodBanksList
+	// clear foodBanksTable and Invalid input string when button is clicked
+	if(output.contains(document.querySelector('#output p'))){
+		console.log(true);
+		output.removeChild(output.lastChild);
+	}
 	clearFoodBanks("");
 	//obtain the food banks
 	fetchCoordinates();
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GunnHacksFoodBank
 Food Bank Project for Gunn Hacks 8.0
+
+This project serves as a CA Food Bank Locator. The user inputs their zip code and finds the 9 food banks located closest to them. There is also a discord bot that provides the user information on food banks in their county.
+
+__________________________
+# APIS
+Our project utilizes the Alpha.CA.gov Food Bank API to locate food banks based on location coordinates. It also uses the Geocode.xyz Forward Geocoding API to convert the user's zip codes into coordinates that can be used to access the Food Bank API.
+
+__________________________
+
+Image Credits - Pixabay.com
