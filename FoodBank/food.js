fa73ba41863e0704d7334a4a6f1de68ff813e69a
RitaliJ <ritaliaarav@gmail.com>
2022-02-05 23:25:43 -0800

    Connected Food Bank API

--- FoodBank/food.js
+++ FoodBank/food.js
@@ -9,13 +9,36 @@ const fetchCoordinates = async () => {
 	try{
 		const zipCode = zipCodeInput.value;
 		const res = await axios.get(`https://geocode.xyz/?geoit=JSON&region=US&locate=${zipCode}`, zipCode);
-		//const res = await axios.get('https://geocode.xyz/?locate=95148&geoit=JSON&region=US');
 		const lat = res.data.latt;
 		const long = res.data.longt;
 		console.log(lat + ", " + long);
+		if(lat == undefined || long == undefined){
+			throw new Exception;
+		}
+		fetchFoodBanks(lat, long);
 	}catch(e){
-		console.log("Error! ", e);
+		console.log("Invalid Input!", e);
+	}
+}
 
+const fetchFoodBanks = async(lat, long) => {
+	try{
+		const res2 = await axios.get(`https://api.alpha.ca.gov/FoodBanks?lat=${lat}&lon=${long}`, lat, long);
+		getFoodBanks(res2.data);
+
+	}catch(e){
+		console.log("Unable to process the request at this moment", e);
+	}
+}
+
+const getFoodBanks = (foodBanksList) => {
+	for(let foodBank of foodBanksList){
+		const name = foodBank.properties.title;
+		const distance = foodBank.properties.distance;
+		const website = foodBank.properties.website;
+		const address = foodBank.properties.address;
+		const city = foodBank.properties.city;
+		console.log(name + " " + distance + "\n" + website + "\n" + address + ", " + city);
 	}
 }
 
@@ -30,3 +53,4 @@ button.addEventListener('click', function(e){
 	fetchCoordinates();
 });
 // clear foodBanksList
+
